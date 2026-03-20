907d4b8bf14042b2f7075e35fe6bcfb0fa677b74
Gyana Ranjan <gyanaranjansahoo174@gmail.com>
2026-03-21 02:34:57 +0530

    testing 2

--- bms/apps/web/app/page.tsx
+++ bms/apps/web/app/page.tsx
@@ -1,9 +1,6 @@
 import Image, { type ImageProps } from "next/image";
 import { Button } from "@repo/ui/button";
 import styles from "./page.module.css";
-import { prisma } from "@repo/db/client";
-
-
 
 
 type Props = Omit<ImageProps, "src"> & {
